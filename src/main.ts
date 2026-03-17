373ccba4de627390bbe180a04f462d5d05a598a0
Sven <sven.wgl@gmail.com>
2026-03-17 21:13:05 +0100

    add: font // add: index.html styling

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
 import './styles/style.scss';
+
+function initEventListenersInput(): void {
+    addClickListener('play-btn', () => {
+        window.location.href = "./src/pages/settings.html";
+    });
+}
+
+function addClickListener(id: string, callback: () => void): void {
+    const element = document.getElementById(id);
+    if (element) {
+        element.addEventListener('click', callback);
+    }
+}
+
+initEventListenersInput();
